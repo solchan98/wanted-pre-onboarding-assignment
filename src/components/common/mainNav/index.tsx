abe712fa993782933ac70f1082603db3def4d789
solchan98 <dev.sol0427@gmail.com>
2022-05-10 22:26:39 +0900

    refactor: 카테고리 아이콘 변경

--- src/components/common/mainNav/index.tsx
+++ src/components/common/mainNav/index.tsx
@@ -1,15 +1,19 @@
-import { Home, Star } from "../../../assets/svgs";
+import { Favorites, Home } from "../../../assets/svgs";
 import useClickNav from "../../../hooks/useClickNav";
 import styles from './mainNav.module.scss';
 
 const MainNav = () => {
 
-  const onClickNav = useClickNav();
+  const [selectState, ,onClickBtn]  = useClickNav('/');
 
   return(
     <nav className={styles.nav}>
-      <button type='button' data-location='/' onClick={onClickNav} aria-label='Main Button'><Home /></button>
-      <button type='button' data-location='/favorites' onClick={onClickNav} aria-label='Favorites Button'><Star /></button>
+      <button type='button' data-location='/' onClick={onClickBtn} aria-label='Main Button'>
+        <Home fill={selectState === '/' ? '#a8a8a8' : '#000'}/>
+      </button>
+      <button type='button' data-location='/favorites' onClick={onClickBtn} aria-label='Favorites Button'>
+        <Favorites fill={selectState === '/favorites' ? '#a8a8a8' : '#000'}/>
+      </button>
     </nav>
   );
 };
--- src/hooks/useClickNav.tsx
+++ src/hooks/useClickNav.tsx
@@ -1,14 +1,20 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const useClickNav = () => {
+type UseClickNavType = [string, React.Dispatch<React.SetStateAction<string>>,(e: React.MouseEvent<HTMLButtonElement>) => void];
+
+const useClickNav = (location: string): UseClickNavType => {
 
   const nav = useNavigate();
+  const [selectState, setSelectState] = useState(location);
 
-  return useCallback((e: React.MouseEvent<HTMLButtonElement>) => {
-    const location = String(e.currentTarget.dataset.location);
-    nav(location);
+  const onClickBtn = useCallback((e: React.MouseEvent<HTMLButtonElement>) => {
+    const loc = String(e.currentTarget.dataset.location);
+    setSelectState(loc);
+    nav(loc);
   }, [nav]);
+
+  return [selectState, setSelectState, onClickBtn];
 };
 
 export default useClickNav;
