ecb55a5e1afb6ac7a08b18b216b68bf704a251dd
solchan98 <dev.sol0427@gmail.com>
2022-05-11 19:39:10 +0900

    feat: 새로 검색 시, 스크롤 맨 처음으로 초기화하는 커스텀 훅 작성

--- src/hooks/useScroll.tsx
+++ src/hooks/useScroll.tsx
@@ -1,21 +1,19 @@
 import { RefObject, UIEvent, useRef } from 'react';
 
-type ReturnTypes = [RefObject<HTMLElement>, (e: UIEvent<HTMLElement>) => void]
+type ReturnTypes = [RefObject<HTMLElement>, (e: UIEvent<HTMLElement>) => void, () => void]
 
 const useScroll = (): ReturnTypes => {
   const scrollRef = useRef<HTMLElement>(null);
 
   const onScroll = (e: UIEvent<HTMLElement>) => {
-
     const currentScroll = e.currentTarget.scrollTop;
     const mainClientHeight = scrollRef.current?.clientHeight || 0;
     const mainClientScrollHeight = scrollRef.current?.scrollHeight || 0;
-    if(currentScroll === mainClientScrollHeight - mainClientHeight) {
-      // TODO API CALL ...
-    }
-    // [3] = [1] + [2]
+    /** TODO API CALL */
   };
-  return [scrollRef, onScroll];
+
+  const scrollToTop = () => scrollRef.current?.scrollTop && scrollRef.current?.scrollTo(0, 0);
+  return [scrollRef, onScroll, scrollToTop];
 };
 
 export default useScroll;
--- src/routes/Main/index.tsx
+++ src/routes/Main/index.tsx
@@ -1,4 +1,4 @@
-import { FormEvent } from 'react';
+import { FormEvent, useEffect } from 'react';
 import { useRecoilState } from 'recoil';
 import MainNav from '../../components/common/mainNav';
 import MovieItem from '../../components/common/movieItem';
@@ -7,11 +7,14 @@ import useScroll from '../../hooks/useScroll';
 import { movieListState } from '../../recoil/atoms';
 import { getMovieListByNameAndPage } from '../../service/movie';
 import styles from './Main.module.scss';
+import cx from 'classnames';
 
 
 const Main = () => {
 
   const [movieList, setMovieList] = useRecoilState(movieListState);
+  
+  const [scrollRef, onScroll, scrollToTop] = useScroll();
 
   const onMovieSearch = (name: string, e?: FormEvent<HTMLFormElement>) => {
     e?.preventDefault();
@@ -19,11 +22,10 @@ const Main = () => {
       getMovieListByNameAndPage(name, 1)
       .then((data) => {
         data.Response !== "False" ? setMovieList(data.Search) : setMovieList([]);
+        scrollToTop();
       });
     }
   };
-  
-  const [scrollRef, onScroll] = useScroll();
 
   return(
     <section>
@@ -31,7 +33,7 @@ const Main = () => {
         <h2>MOVIES</h2>
         <SearchBar onSearch={onMovieSearch}/>
       </header>
-      <main ref={scrollRef} className={styles.main} onScroll={onScroll}>
+      <main ref={scrollRef} className={cx(styles.main, movieList.length && styles.on)} onScroll={onScroll}>
         <ul>
           { movieList.length !== 0 && movieList.map((movie, index) => 
             <li key={`main_movie_${movie.imdbId}_${index + 1}`}>
